12db3e780007d11bcda4fde8644751abedb26dfd
sakuran_hanabir <105345796+sakusdev@users.noreply.github.com>
2023-04-12 17:38:49 +0900

    Update script.js

--- tools/internetspeed/script.js
+++ tools/internetspeed/script.js
@@ -4,7 +4,7 @@ const speedDisplay = document.getElementById("speedDisplay");
 startBtn.addEventListener("click", function() {
     const xhr = new XMLHttpRequest();
     const startTime = Date.now();
-    xhr.open("GET", "https://github.com/sakusdev/internetspeedfile_/raw/main/inter.bin", true);
+    xhr.open("GET", "https://ucb2d818b25a7f794d8080ca2ce9.dl.dropboxusercontent.com/cd/0/get/B6BkJPhkizTx4-g_LAHSoCEWrALUmO456C8ohna255hFj5hi241H8DnDw-Td7r-2CAZ4ZuDM_gILUOzNc2c21SKIHVGovEAH-MX-nvU3GNUUgjRs7pimwGwKgKi5f7-UDBOdL1EUDMQFf9ieNNGcGEeTKUX4HnHba4dZs92cHc6QYBahY4fFvfiDdswJnXrX6Qw/file#", true);
     xhr.responseType = "blob";
     xhr.onload = function() {
         const endTime = Date.now();
